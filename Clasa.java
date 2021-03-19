797eb3c812f649c65d0203cdcf447c7236001a85
emy953 <80955889+emy953@users.noreply.github.com>
2021-03-19 12:47:53 +0200

    Create Clasa.java

---
+++ Clasa.java
@@ -0,0 +1,16 @@
+class Clasa{
+ private int x,y;
+  public Clasa(int a,int b)
+  {
+    x=a;
+    y=b;
+  }
+  public int getx()
+  {
+   return x; 
+  }
+  public int gety()
+  {
+    return y;
+  }
+}
